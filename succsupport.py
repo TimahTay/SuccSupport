b4a809066828d08b71e64beeb2b9c5c2e5fead19
Unknown <tjtatex@gmail.com>
2018-07-19 15:05:19 -0500

    Updated to python 3.5.2 for pi

--- succsupport.py
+++ succsupport.py
@@ -1,65 +1,65 @@
 from Adafruit_MotorHAT import Adafruit_DCMotor
 from Adafruit_MotorHAT import Adafruit_MotorHAT
-import Tkinter
+import tkinter
 import datetime
 
 class MyGUI:
     
     def __init__(self):
         #creates a new window
-        self.window = Tkinter.Tk()
+        self.window = tkinter.Tk()
         self.window.title("Succ Support")
         self.window.geometry("800x480")
         self.window.configure(background="#F4F4F2")
         #self.window.attributes("-fullscreen", True)
 
         #widgets
-        self.photo = Tkinter.PhotoImage(file="SuccSupportBanner.gif")
-        self.label0 = Tkinter.Label(self.window, image=self.photo)
+        self.photo = tkinter.PhotoImage(file="SuccSupportBanner.gif")
+        self.label0 = tkinter.Label(self.window, image=self.photo)
         self.label0.pack()
 
         self.label1Text = 'Time: '
-        self.label1 = Tkinter.Label(self.window, text=self.label1Text, fg="#4C4C4C", bg="#F4F4F2")
+        self.label1 = tkinter.Label(self.window, text=self.label1Text, fg="#4C4C4C", bg="#F4F4F2")
         self.label1.config(font=("Arial", 15))
         self.label1.place(relx=0.5, rely=0.375, anchor='center')
 
         #self.label2Text = 'Watered: '
-        #self.label2 = Tkinter.Label(self.window, text=self.label2Text, fg="#4C4C4C", bg="#F4F4F2")
+        #self.label2 = tkinter.Label(self.window, text=self.label2Text, fg="#4C4C4C", bg="#F4F4F2")
         #self.label2.config(font=("Arial", 15))
         #self.label2.place(relx=0.5, rely=0.625, anchor='center')
 
         self.label3Text = 'Next Water: '
-        self.label3 = Tkinter.Label(self.window, text=self.label3Text, fg="#4C4C4C", bg="#F4F4F2")
+        self.label3 = tkinter.Label(self.window, text=self.label3Text, fg="#4C4C4C", bg="#F4F4F2")
         self.label3.config(font=("Arial", 15))
         self.label3.place(relx=0.5, rely=0.5, anchor='center')
 
-        self.scheduleEntry = Tkinter.Entry(self.window, width=2, fg="#4C4C4C")
+        self.scheduleEntry = tkinter.Entry(self.window, width=2, fg="#4C4C4C")
         self.scheduleEntry.config(font=("Arial", 15))
         self.scheduleEntry.place(relx=0.45, rely=0.625, anchor='e')
         
-        self.label4 = Tkinter.Label(self.window, text='Days Between Water: ', fg="#4C4C4C", bg="#F4F4F2")
+        self.label4 = tkinter.Label(self.window, text='Days Between Water: ', fg="#4C4C4C", bg="#F4F4F2")
         self.label4.config(font=("Arial", 15))
         self.label4.place(relx=0.4, rely=0.625, anchor='e')
         
         #400ml/min
         #500ml in a bottle
-        self.waterVolumeSlider = Tkinter.Scale(self.window, from_=0, to=500, orient='horizontal', length=200, fg="#4C4C4C", bg="#F4F4F2")
+        self.waterVolumeSlider = tkinter.Scale(self.window, from_=0, to=500, orient='horizontal', length=200, fg="#4C4C4C", bg="#F4F4F2")
         self.waterVolumeSlider.config(font=("Arial", 12))
         self.waterVolumeSlider.place(relx=0.55, rely=0.625, anchor='w')
         
-        self.label5 = Tkinter.Label(self.window, text='ML', fg="#4C4C4C", bg="#F4F4F2")
+        self.label5 = tkinter.Label(self.window, text='ML', fg="#4C4C4C", bg="#F4F4F2")
         self.label5.config(font=("Arial", 15))
         self.label5.place(relx=0.825, rely=0.625, anchor='w')
         
-        self.submitButton = Tkinter.Button(self.window, command=self.submitButtonPress, text='Submit', fg="#4C4C4C", bg="#F4F4F2")
+        self.submitButton = tkinter.Button(self.window, command=self.submitButtonPress, text='Submit', fg="#4C4C4C", bg="#F4F4F2")
         self.submitButton.config(font=("Arial", 12))
         self.submitButton.place(relx=0.475, rely=0.75, anchor='e')
         
-        self.cancelButton = Tkinter.Button(self.window, command=self.cancelButtonPress, text='Cancel', fg="#4C4C4C", bg="#F4F4F2")
+        self.cancelButton = tkinter.Button(self.window, command=self.cancelButtonPress, text='Cancel', fg="#4C4C4C", bg="#F4F4F2")
         self.cancelButton.config(font=("Arial", 12))
         self.cancelButton.place(relx=0.525, rely=0.75, anchor='w')
         
-        self.waterButton = Tkinter.Button(self.window, command=self.waterButtonPress, text='Water', fg="#001ECC", bg="#F4F4F2")
+        self.waterButton = tkinter.Button(self.window, command=self.waterButtonPress, text='Water', fg="#001ECC", bg="#F4F4F2")
         self.waterButton.config(font=("Arial", 12))
         self.waterButton.place(relx=0.5, rely=0.875, anchor='center')
 
@@ -83,7 +83,7 @@ class MyGUI:
     def water(self):
         #waters plant, updates then calls for updating next water time
         self.wateredTime = str(datetime.datetime.now())[:-7]
-        print "watering..." + " | " + str(self.wateredTime) 
+        print("watering..." + " | " + str(self.wateredTime)) 
         
         #self.label2.configure(text = 'Watered: ' + self.wateredTime)
         self.updateWaterDateFile()
@@ -104,14 +104,14 @@ class MyGUI:
             self.water()
             file = open("waterDate.txt", "w")
             file.write(str(datetime.datetime.now())[:-7])
-            print "no water date detected, setting to now"
+            print("no water date detected, setting to now")
             file.close()
         else:
             file = open("waterDate.txt", "r")
             self.wateredTime = str(file.read())
             #self.label2.configure(text = 'Watered: ' + self.wateredTime)
             file.close()
-            print "water date loaded"
+            print("water date loaded")
         
         #if there is not a water schedule, sets it to 7
         file = open("waterSchedule.txt", "r")
@@ -119,9 +119,9 @@ class MyGUI:
             file.close()
             file = open("waterSchedule.txt", "w")
             file.write('7')
-            print "no schedule detected, set to 7"
+            print("no schedule detected, set to 7")
         else:
-            print "schedule loaded"
+            print("schedule loaded")
         file.close()
         
         self.updateNextWater()
@@ -130,18 +130,18 @@ class MyGUI:
         file = open("waterVolume.txt", "r")
         file.seek(0,0)
         self.wvo = file.read()
-        print "water volume file: " + self.wvo
+        print("water volume file: " + self.wvo)
         if(len(self.wvo) > 0):
-            print "water volume loaded"
+            print("water volume loaded...")
             if(int(self.wvo) >= 0 and int(self.wvo) < 401):
                 self.waterVolume = int(self.wvo)
-                print "WATER VOLUME WAS SET BABY " + self.wvo
+                print("water volume set to " + self.wvo)
             else:
-                print self.wvo + " water volume value extraneous, set to 200"
+                print(self.wvo + " water volume value extraneous, set to 200")
                 self.waterVolume = 200
                 self.updateWaterVolumeFile()
         else:
-            print "not volume found, set to 200"
+            print("not volume found, set to 200")
             self.waterVolume = 200
             self.updateWaterVolumeFile()
             
@@ -160,11 +160,11 @@ class MyGUI:
         #sets timedelta based off of schedule file
         file = open("waterSchedule.txt", 'r')
         self.scheduleFileOutput = file.read()
-        print "schedule file: " + self.scheduleFileOutput
+        print("schedule file: " + self.scheduleFileOutput)
         if(len(self.scheduleFileOutput) >= 1):
             self.daysDelta = int(self.scheduleFileOutput)
         else:
-            print "no schedule file, set to 7"
+            print("no schedule file, set to 7")
             self.daysDelta = 7
         file.close()
 
@@ -218,7 +218,7 @@ class MyGUI:
         
     def updateWaterVolumeFile(self):
         file = open("waterVolume.txt", "w")
-        print "writing volume to file: " + str(self.waterVolume)
+        print("writing volume to file: " + str(self.waterVolume))
         file.write(str(self.waterVolume))
         file.close()
         
